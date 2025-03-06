c3e6dbafc65c19120e6094d07c7854719e5cad24
Aamir Khan <aamirksfg@gmail.com>
2025-03-05 17:46:35 -0800

    styling login dialog

--- Pixelate/User_Authentication/auth_dialogs.py
+++ Pixelate/User_Authentication/auth_dialogs.py
@@ -4,7 +4,7 @@ from PyQt6.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout, QPushButton,
                              QLabel, QListWidget, QListWidgetItem, QDialog,
                              QLineEdit, QMessageBox, QGridLayout, QScrollArea,
                              QApplication)
-from PyQt6.QtGui import QPixmap, QColor
+from PyQt6.QtGui import QPixmap, QColor, QFontDatabase, QFont
 from PyQt6.QtCore import Qt
 
 class LoginDialog(QDialog):
@@ -12,10 +12,19 @@ class LoginDialog(QDialog):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Pixelate - Login")
+        # Hiding our system taskbar and keeping our dialog on top.
+        self.setWindowFlag(Qt.WindowType.WindowStaysOnTopHint | Qt.WindowType.FramelessWindowHint)
         
         # Setting up our layout.
         layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(20)
+
+        # Create a header label.
+        self.header_label = QLabel("Gallery - Login")
+        self.header_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.header_label.setStyleSheet(self.get_header_style())
+        layout.addWidget(self.header_label)
 
         # Create a label for the user to enter their email.
         self.email_label = QLabel("Email:")
@@ -33,14 +42,25 @@ class LoginDialog(QDialog):
         self.register_button = QPushButton("Register")
         self.register_button.clicked.connect(self.register)
 
+        # Create a button to close the dialog.
+        self.close_button = QPushButton("Close")
+        self.close_button.clicked.connect(self.reject)
+
+        # A layout to hold our buttons.
+        button_layout = QHBoxLayout()
+        button_layout.setContentsMargins(0, 0, 0, 0)
+        button_layout.setSpacing(2)
+        button_layout.addWidget(self.login_button)
+        button_layout.addWidget(self.register_button)
+
         # Add all of our widgets to the layout.
         layout.addWidget(self.email_label)
         layout.addWidget(self.email_input)
         layout.addWidget(self.password_label)
         layout.addWidget(self.password_input)
-        layout.addWidget(self.login_button)
-        layout.addWidget(self.register_button)
-
+        layout.addLayout(button_layout)
+        layout.addWidget(self.close_button)
+        self.setStyleSheet(self.get_style())
         self.setLayout(layout)
 
     # A method to switch to a registration dialog.
@@ -49,6 +69,86 @@ class LoginDialog(QDialog):
         self.close()
         register_dialog = RegisterDialog()
         register_dialog.exec()
+
+    # Header styling.
+    def get_header_style(self):
+        return f'''
+            QLabel {{
+                background-color: purple;
+                color: white;
+                padding: 10px;
+                margin: 0px;
+                font-size: 20px;
+            }}
+            '''
+
+    # Default styling.
+    def get_style(self):
+        return f'''
+            QWidget {{
+                background-color: lightgray;
+                font-family: {self.get_font().family()};
+                color: black;
+            }}
+            QLabel {{
+                color: black;
+                margin-left: 10px;
+                margin-right: 10px;
+                font-size: 14px;
+            }}
+            QLineEdit {{
+                background-color: white;
+                color: black;
+                padding: 5px;
+                margin-left: 10px;
+                margin-right: 10px;
+            }}
+            QTextEdit {{
+                background-color: white;
+                color: black;
+                padding: 5px;
+                margin-left: 10px;
+                margin-right: 10px;
+            }}
+            QPushButton {{
+                color: black;
+                background-color: white;
+                padding: 10px;
+                margin-left: 10px;
+                margin-right: 10px;
+                margin-bottom: 10px;
+                border: 2px solid #A9A9A9;
+                border-radius: 10px;
+            }}
+            QPushButton:hover {{
+                color: white;
+                background-color: purple;
+                border: 2px solid white;
+            }}
+            QPushButton:pressed {{
+                color: white;
+                background-color: #4B0082;
+                border: 2px solid white
+            }}
+        '''
+    
+    # A method to get our pixelated font.
+    def get_font(self):
+
+        # Setting up our pixelated font:
+        font_path = "fonts/Press_Start_2P/PressStart2P-Regular.ttf"
+
+        # Adding our pixelated font to the QFontDatabase.
+        font_id = QFontDatabase.addApplicationFont(font_path)
+        
+        # If the font was loaded successfully, we'll use it for our button text.
+        if font_id != -1:
+            pixelated_font = QFont("Press Start 2P")
+        else:
+            # If the font wasn't loaded, we'll use the default application font.
+            pixelated_font = QFont()
+
+        return pixelated_font
 
 class RegisterDialog(QDialog):
 
@@ -89,6 +189,7 @@ class RegisterDialog(QDialog):
         layout.addWidget(self.password_label)
         layout.addWidget(self.password_input)
         layout.addWidget(self.register_button)
+        layout.addWidget(self.login_button)
 
         self.setLayout(layout)
 
@@ -100,10 +201,10 @@ class RegisterDialog(QDialog):
         login_dialog.exec()
 
 
-# Testing our login dialog.
-# app = QApplication([])
-# login_dialog = LoginDialog()
-# login_dialog.exec()
+#Testing our login dialog.
+app = QApplication([])
+login_dialog = LoginDialog()
+login_dialog.exec()
 
 # Testing our registration dialog.
 # app = QApplication([])
